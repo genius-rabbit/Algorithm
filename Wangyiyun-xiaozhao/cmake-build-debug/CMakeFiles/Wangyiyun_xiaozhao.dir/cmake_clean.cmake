7294c0a8e56da8347f739d48842f1039a0450db4
geniusrabbit <U201613566@hust.edu.cn>
2017-10-11 12:55:17 +0800

    10-11

---
+++ Wangyiyun-xiaozhao/cmake-build-debug/CMakeFiles/Wangyiyun_xiaozhao.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Wangyiyun_xiaozhao.dir/main.c.o"
+  "Wangyiyun_xiaozhao.pdb"
+  "Wangyiyun_xiaozhao"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Wangyiyun_xiaozhao.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Wangyiyun-xiaozhao/main.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+
+/**题目来源：算法爱好者：网易2017校园招聘
+ *
+ * 题目描述：
+ * 给定一个数组，除了一个数出现了一次之外其余数都出现了三次，找出出现一次的那个数字
+ * 题目要求：线性时间复杂度，不能使用额外的空间
+ * 样例输入：
+ *      4
+ *      0005
+ * 样例输出：
+ *      5
+ *
+ * 参考评论中的解法：
+ * 采用三进制加法
+ *
+ */
+
+int Findtheonlyone(int* num,int numsSize){
+    int last=num[0],next;
+    int lastnumber[20]={0},nextnumber[20]={0};
+    int i=0,j;
+    if(last==0){
+        lastnumber[i]=last;
+    }else{
+        while (last>0){
+            lastnumber[i]=last%3;
+            last=last/3;
+            i++;
+        }
+    }
+
+
+    for(i=1;i<numsSize;i++){
+        next=num[i];
+        j=0;
+        if(next==0){
+            nextnumber[j]=next;
+        }else{
+            while (next>0){
+                nextnumber[j]=next%3;
+                next=next/3;
+                j++;
+            }
+        }
+
+        for(j=0;j<20;j++){
+            if(lastnumber[j]+nextnumber[j]<3){
+                lastnumber[j]=lastnumber[j]+nextnumber[j];
+            }else{
+                lastnumber[j]=(lastnumber[j]+nextnumber[j])%3;
+            }
+        }
+        
+    }
+    int sum=0;
+
+    for (j=0;j<20;j++) {
+        sum=lastnumber[i]*3+sum;
+    }
+
+    return sum;
+}
+
+
+int main() {
+    int result;
+    int num[4]={0,0,0,4};
+    result=Findtheonlyone(num,4);
+    printf("%d",result);
+    return 0;
+}
